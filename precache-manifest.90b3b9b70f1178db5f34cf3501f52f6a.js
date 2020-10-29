d053d21deb18e210582875cbfea437fd509361e1
CrioDo <rahul@criodo.com>
2020-10-29 20:58:57 +0530

    Updates

---
+++ precache-manifest.90b3b9b70f1178db5f34cf3501f52f6a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11cdba21d4d6ccd73a9275ad96c908fb",
+    "url": "/portfolio-omkar-16bit1064-abes-ac/index.html"
+  },
+  {
+    "revision": "6eeb2370db9b3fc2069d",
+    "url": "/portfolio-omkar-16bit1064-abes-ac/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "8ed20548719483371f93",
+    "url": "/portfolio-omkar-16bit1064-abes-ac/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "6eeb2370db9b3fc2069d",
+    "url": "/portfolio-omkar-16bit1064-abes-ac/static/js/2.8e0d2b32.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/portfolio-omkar-16bit1064-abes-ac/static/js/2.8e0d2b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed20548719483371f93",
+    "url": "/portfolio-omkar-16bit1064-abes-ac/static/js/main.3c01315e.chunk.js"
+  },
+  {
+    "revision": "891051fdb3040b1d91bd",
+    "url": "/portfolio-omkar-16bit1064-abes-ac/static/js/runtime-main.32d76133.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/portfolio-omkar-16bit1064-abes-ac/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/portfolio-omkar-16bit1064-abes-ac/static/media/crio-logo.63be71d0.svg"
+  }
+]);
